aefa857869b1d1235a50211f48fef22f2eb35683
Brian O'Meara <omeara.brian@gmail.com>
2020-02-25 23:19:36 -0500

    package can now do taxonomy automatically I think

--- R/closesamples.R
+++ R/closesamples.R
@@ -48,3 +48,74 @@ GetClosestSamples <- function(n, phy_full, taxa_possible, replace=TRUE) {
   }
   return(chosen.df)
 }
+
+#' Get enclosing taxonomy for a focal tree
+#'
+#' If you don't have a taxonomy tree for the focal tree, this will find one
+#' @param tips A vetor of taxon names
+#' @return A phylogeny of the enclosing taxonomy
+#' @export
+GetEnclosingTaxonomy <- function(phy_focal) {
+  # OTOL attempt, but after all this can only return trees of 25K taxa
+
+  # ott_ids <- c()
+  # focal_tips <- phy_focal$tip.label
+  # tips_splits <- split(focal_tips, ceiling(seq_along(focal_tips)/2000))
+  # for (i in sequence(length(tips_splits))) {
+  #   ott_ids <- c(ott_ids,rotl::tnrs_match_names(tips_splits[[i]])$ott_id)
+  # }
+  # ott_ids <- ott_ids[!is.na(ott_ids)]
+  # ott_ids_good <- rep(NA, length(ott_ids))
+  # for (i in seq_along(ott_ids)) {
+  #   attempt <- NULL
+  #   try(attempt <- rotl::tol_node_info(ott_ids[i]), silent=TRUE)
+  #   if(!is.null(attempt)) {
+  #     ott_ids_good[i] <- ott_ids[i]
+  #   }
+  # }
+  # crown_node <- rotl::tol_mrca(ott_ids=ott_ids_good[!is.na(ott_ids_good)])
+  # phy_full <- rotl::tol_subtree(node_id=crown_node$mrca$node_id, label_format="name")
+
+  classification_info <- vector(mode = "list", length = length(tips))
+  for (i in seq_along(tips)) {
+   classification_info[i] <- taxize::classification(tips[i], db = "itis")
+   Sys.sleep(1)
+  }
+  attr(classification_info, "db") <- "itis"
+  attr(classification_info, "class") <- "classification"
+  names(classification_info) <- phy_focal$tip.label
+  classification_info <- classification_info[!is.na(classification_info)]
+  ids <- lapply(classification_info, "[[", "id")
+  ids_count <- table(unlist(ids))
+  tipmost_mrca <- NA
+  for (i in seq_along(ids[[1]])) {
+    if(ids_count[ids[[1]][i]]==length(classification_info)) {
+      tipmost_mrca <- ids[[1]][i]
+    }
+  }
+  all_tips <- taxize::downstream(as.numeric(tipmost_mrca), db="itis", downto="species")
+  all_classifications <- taxize::classification(as.numeric(all_tips[[1]][1]$tsn), db="itis")
+  phy_full <- ape::stree(n=nrow(all_tips[[1]]), type="star", tip.label=all_tips[[1]]$taxonname)
+  try(phy_full <- taxize::class2tree(all_classifications))
+  return(phy_full)
+}
+
+#' Return a sampled tree
+#'
+#' @param phy_focal The tree to subsample
+#' @param n The number of taxa to include
+#' @param phy_full The larger tree giving relationships
+#' @replace If TRUE, will allow getting the same taxon more than once. If false, forbids this. Both cases return n taxa
+#' @return A phylo object where taxa are sampled based on representing flat sampling from taxonomy
+#' @export
+SubsampleTree <- function(phy_focal, n, phy_full=NULL, replace=TRUE) {
+  if(is.null(phy_full)) {
+    phy_full <- GetEnclosingTaxonomy(phy_focal$tip.label)
+    if(ape::Nnode(phy_full)==1) {
+      warning("Taxonomy tree is completely unresolved")
+    }
+  }
+  samples <- GetClosestSamples(n=n, phy_full=phy_full, taxa_possible=phy_focal$tip.label, replace=replace)
+  phy_sample <- ape::keep.tip(phy_focal, samples$closest)
+  return(phy_sample)
+}
--- tests/testthat/test-similarity.R
+++ tests/testthat/test-similarity.R
@@ -9,7 +9,6 @@ test_that("get similarity works", {
 test_that("get data.frame of closest taxa works", {
   phy <- ape::rcoal(10)
   focal_species <- phy$tip.label[1:5]
-  closest.df <- GetClosestSamples(n=3, phy, focal_species) 
-  expect_equal(nrow(similarity_matrix), 10)
-  expect_equal(ncol(similarity_matrix), 4)
+  closest.df <- GetClosestSamplesDF(n=3, phy, focal_species)
+  expect_equal(nrow(closest.df), 3)
 })
